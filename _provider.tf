3e17d18bce75f8fb3ac4064240ee99f07c7955fa
Matias Stahl <stahlmatias@gmail.com>
2025-07-03 11:36:30 -0300

    try digital ocean as cloud provider

---
+++ _provider.tf
@@ -0,0 +1,16 @@
+variable "digitalocean_token" {}
+
+terraform {
+  required_version = ">= 0.14.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+    }
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.digitalocean_token
+}
